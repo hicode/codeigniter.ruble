2800d9898dbb078b36ba2e2453ab9f36dcbb8236
unknown <Home@.(none)>
2012-09-01 02:09:23 +0800

    update

--- templates/templates.rb
+++ templates/templates.rb
@@ -1,8 +1,14 @@
 require 'ruble'
 
-
 template 'Controller Template' do |t|
   t.filetype = "*.php"
   t.location = "templates/tpl_Controller.php"
   t.replace_parameters = false
+end
+
+project_template "Sample Project Template" do |t|
+	t.type = :web
+	t.location = "git://github.com/EllisLab/CodeIgniter.git"
+	t.description = "Codeigniter 2.1.2 Project"
+	t.icon = "codeigniter.png"
 end
